70d4a444513977d8db8fd8f77418fa5b4c7c86c1
Kateryna <ketpolon@gmail.com>
2026-06-03 13:39:49 -0400

    13:34 3th June 2026, add comments in analitycs.py

--- analitycs.py
+++ analitycs.py
@@ -1,4 +1,7 @@
+# import liblaries
 import pandas as pd
 import numpy as np
+# create dataFrame from dictionary
 df = pd.DataFrame({'Name':['Ira', 'Mark', 'Ostap'], 'Age':['24', '31', '26']})
-df.head()
+# print df.head()
+print(df.head())
