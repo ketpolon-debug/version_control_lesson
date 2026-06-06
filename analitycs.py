a883993d5d59d5da146f0a7546d897cad8dba816
Kateryna <ketpolon@gmail.com>
2026-06-05 21:31:00 -0400

    add barplot name - age

--- analitycs.py
+++ analitycs.py
@@ -1,7 +1,11 @@
 # import liblaries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 # create dataFrame from dictionary
-df = pd.DataFrame({'Name':['Ira', 'Mark', 'Ostap'], 'Age':['24', '31', '26']})
+df = pd.DataFrame({'Name':['Ira', 'Mark', 'Ostap'], 'Age':[24, 31, 26]})
 # print df.head()
 print(df.head())
+sns.barplot = sns.barplot(data = df, x = 'Name', y = 'Age')
+plt.show()
